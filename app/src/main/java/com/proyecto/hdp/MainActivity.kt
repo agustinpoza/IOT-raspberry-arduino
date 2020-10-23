2c2a8280747d0319d2d3c8066e039bdffdca761f
AGUSTIN <agustin.pozaai@gmail.com>
2020-10-23 17:15:04 -0300

    avanzamos con la comunicacion con el database

--- app/src/main/java/com/proyecto/hdp/MainActivity.kt
+++ app/src/main/java/com/proyecto/hdp/MainActivity.kt
@@ -2,19 +2,32 @@ package com.proyecto.hdp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    val database = Firebase.database
+
+    var refCasa = database.getReference("home")
+   lateinit var refLuz : DatabaseReference
+   lateinit var refLuzCocina : DatabaseReference
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val database = Firebase.database
         val myRef = database.getReference("message")
-        val mensaje2 = database.getReference("casa")
+        val mensaje2 = myRef.child("message")
 
         mensaje2.setValue("prender luces")
-        myRef.setValue("Hello, World!")
+
+        refLuz = refCasa.child("luces")
+        refLuzCocina = refLuz.child("luz_cocina")
+
+
+
     }
 }
