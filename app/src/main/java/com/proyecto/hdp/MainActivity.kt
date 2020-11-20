0ecf9f20531479bea44e17d7e9e323f01869ec59
agustinpoza <71528107+agustinpoza@users.noreply.github.com>
2020-11-20 13:23:56 -0300

    implementamos tanto la escritura como la lectura de nuestra base de datos

--- app/src/main/java/com/proyecto/hdp/MainActivity.kt
+++ app/src/main/java/com/proyecto/hdp/MainActivity.kt
@@ -2,35 +2,70 @@ package com.proyecto.hdp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import android.widget.CompoundButton
+import android.widget.TextView
+import android.widget.ToggleButton
+import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    val database = Firebase.database
+    private var database = Firebase.database
 
     var refCasa = database.getReference("home")
-   lateinit var refLuz : DatabaseReference
-   lateinit var refLuzCocina : DatabaseReference
-
+    lateinit var refLuz : DatabaseReference
+    lateinit var refLuzCocina : DatabaseReference
+    var btnToggle : ToggleButton? = null
+    var textEstadoPulsador : TextView? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val myRef = database.getReference("message")
-        val mensaje2 = myRef.child("message")
-
-        mensaje2.setValue("prender luces")
 
         refLuz = refCasa.child("luces")
         refLuzCocina = refLuz.child("luz_cocina")
+        refLuzCocina.setValue(true)
 
-        botonPut.setOnClickListener {
-            refLuzCocina.setValue("True")
-        }
+        btnToggle = findViewById<ToggleButton>(R.id.toggleButton)
+        textEstadoPulsador = findViewById<TextView>(R.id.textEstadoPulsador)
+
+        luzControl(refLuzCocina, btnToggle)
+
+
 
     }
+    //creamos una funcion para controlar refLuzCocina
+    private fun luzControl(refLuzCocina: DatabaseReference?, btnToggle: ToggleButton?) {
+        //funcion para que el programe escuche cada cambio del boton y lo envie al database
+        btnToggle?.setOnCheckedChangeListener { _, isChecked ->
+            refLuzCocina?.setValue(
+                isChecked
+            )
+        }
+        // creamos un escucha para refLuzCocina
+        refLuzCocina?.addValueEventListener(object : ValueEventListener {
+            //funcion que se ejecuta cada vez que toma una datasnapshot
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //creamos un valor al cual le asignamos el dato de nuestro snapshot
+                val estadoLuz = snapshot.value as Boolean
+                //hacemos que nuestro boton tome el estado del valor anteriormente asignado
+                btnToggle?.isChecked = estadoLuz
+                //se grafcara en el boton si esta encendido o apagado
+                if (estadoLuz) {
+                    toggleButton.textOn = "ENCENDIDO"
+                }
+                else {
+                    toggleButton.textOff = "APAGADO"
+                }
+            }
+            //error en el firebase
+            override fun onCancelled(error: DatabaseError) {println("error Firebase refLuzCocina")}
+        })
+
+    }
+
 }
+
+
